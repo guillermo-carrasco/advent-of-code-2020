f05e3524a55e3d8b2d63ab93ba28b66a94c7da85
guillermo-carrasco <guillermo.carrasco@izettle.com>
2020-12-01 14:46:53 +0100

    Generates test cases for day 1 - part 1

--- tests/test_day_1.py
+++ tests/test_day_1.py
@@ -1,3 +1,5 @@
+import random
+
 from solutions.day_1 import Day1
 
 
@@ -11,3 +13,25 @@ def test_part_2(day_1_input):
     solver = Day1(day_1_input)
 
     assert solver.part_2(2020) == 241861950
+
+
+def generate_test_case(length, max_int):
+    numbers = list(range(2021))
+    i = 0
+    j = len(numbers) - 1
+    possible_pairs = []
+    while i <= j:
+        possible_pairs.append((numbers[i], numbers[j]))
+        i += 1
+        j -= 1
+
+    a, b = possible_pairs[random.randint(0, len(possible_pairs) - 1)]
+    test_case = [a, b]
+    for _ in range(length - 2):
+        n = random.randint(0, max_int)
+        while n == a or n == b:
+            n = random.randint(0, max_int)
+        test_case.append(n)
+
+    with open('test.txt', 'w') as f:
+        f.writelines([str(n) + '\n' for n in test_case])
