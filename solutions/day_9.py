405a7e97acc1e8a6e57159b39b3f736e75d0f499
guillermo-carrasco <guillermo.carrasco@izettle.com>
2020-12-09 15:05:45 +0100

    Adds solutions to day 9 part 2

--- solutions/day_9.py
+++ solutions/day_9.py
@@ -58,6 +58,41 @@ number that does not follow this rule is 127.
 
 The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble)
 which is not the sum of two of the 25 numbers before it. What is the first number that does not have this property?
+
+--- Part Two ---
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must find a contiguous
+set of at least two numbers in your list which sum to the invalid number from step 1.
+
+Again consider the above example:
+
+    35
+    20
+    15
+    25
+    47
+    40
+    62
+    55
+    65
+    95
+    102
+    117
+    150
+    182
+    127
+    219
+    299
+    277
+    309
+    576
+
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course,
+the contiguous set of numbers in your actual list might be much longer.)
+
+To find the encryption weakness, add together the smallest and largest number in this contiguous range; in this example,
+these are 15 and 47, producing 62.
+
+What is the encryption weakness in your XMAS-encrypted list of numbers?
 """
 
 
@@ -92,5 +127,15 @@ class Day9(object):
             left += 1
             right += 1
 
-    def part_2(self):
-        return 0
+    def part_2(self, preamble=25):
+        vulnerability = self.part_1(preamble=preamble)
+        left = right = 0
+        _sum = self.code[left]
+        while _sum != vulnerability:
+            if _sum < vulnerability:
+                right += 1
+            elif _sum > vulnerability:
+                left += 1
+            _sum = sum(self.code[left:right])
+
+        return min(self.code[left:right]) + max(self.code[left:right])
--- tests/test_day_9.py
+++ tests/test_day_9.py
@@ -7,7 +7,7 @@ def test_part_1(day_9_input):
     assert solver.part_1(preamble=5) == 127
 
 
-def test_part_2(day_8_input):
+def test_part_2(day_9_input):
 
-    solver = Day9(day_8_input)
-    assert solver.part_2() == 62
+    solver = Day9(day_9_input)
+    assert solver.part_2(preamble=5) == 62
