a0c3469ff3e571e3f96bee6bd7f48dcf9c046b8e
guillermo-carrasco <guillermo.carrasco@izettle.com>
2020-12-13 16:42:35 +0100

    Adds solutions for Dat 13 part 2

--- solutions/day_13.py
+++ solutions/day_13.py
@@ -60,8 +60,76 @@ The earliest bus you could take is bus ID 59. It doesn't depart until timestamp 
 What is the ID of the earliest bus you can take to the airport multiplied by the number of minutes you'll need to wait
 for that bus?
 
+--- Part Two ---
+The shuttle company is running a contest: one gold coin for anyone that can find the earliest timestamp such that the
+first bus ID departs at that time and each subsequent listed bus ID departs at that subsequent minute. (The first line
+in your input is no longer relevant.)
 
+For example, suppose you have the same list of bus IDs as above:
+
+7,13,x,x,59,x,31,19
+An x in the schedule means there are no constraints on what bus IDs must depart at that time.
+
+This means you are looking for the earliest timestamp (called t) such that:
+
+Bus ID 7 departs at timestamp t.
+Bus ID 13 departs one minute after timestamp t.
+There are no requirements or restrictions on departures at two or three minutes after timestamp t.
+Bus ID 59 departs four minutes after timestamp t.
+There are no requirements or restrictions on departures at five minutes after timestamp t.
+Bus ID 31 departs six minutes after timestamp t.
+Bus ID 19 departs seven minutes after timestamp t.
+The only bus departures that matter are the listed bus IDs at their specific offsets from t. Those bus IDs can depart
+at other times, and other bus IDs can depart at those times. For example, in the list above, because bus ID 19 must
+depart seven minutes after the timestamp at which bus ID 7 departs, bus ID 7 will always also be departing with bus ID
+19 at seven minutes after timestamp t.
+
+In this example, the earliest timestamp at which this occurs is 1068781:
+
+time     bus 7   bus 13  bus 59  bus 31  bus 19
+1068773    .       .       .       .       .
+1068774    D       .       .       .       .
+1068775    .       .       .       .       .
+1068776    .       .       .       .       .
+1068777    .       .       .       .       .
+1068778    .       .       .       .       .
+1068779    .       .       .       .       .
+1068780    .       .       .       .       .
+1068781    D       .       .       .       .
+1068782    .       D       .       .       .
+1068783    .       .       .       .       .
+1068784    .       .       .       .       .
+1068785    .       .       D       .       .
+1068786    .       .       .       .       .
+1068787    .       .       .       D       .
+1068788    D       .       .       .       D
+1068789    .       .       .       .       .
+1068790    .       .       .       .       .
+1068791    .       .       .       .       .
+1068792    .       .       .       .       .
+1068793    .       .       .       .       .
+1068794    .       .       .       .       .
+1068795    D       D       .       .       .
+1068796    .       .       .       .       .
+1068797    .       .       .       .       .
+In the above example, bus ID 7 departs at timestamp 1068788 (seven minutes after t). This is fine; the only requirement
+on that minute is that bus ID 19 departs then, and it does.
+
+Here are some other examples:
+
+The earliest timestamp that matches the list 17,x,13,19 is 3417.
+67,7,59,61 first occurs at timestamp 754018.
+67,x,7,59,61 first occurs at timestamp 779210.
+67,7,x,59,61 first occurs at timestamp 1261476.
+1789,37,47,1889 first occurs at timestamp 1202161486.
+However, with so many bus IDs in your list, surely the actual earliest timestamp will be larger than 100000000000000!
+
+What is the earliest timestamp such that all of the listed bus IDs depart at offsets matching their positions in the
+list?
 """
+import numpy as np
+
+from functools import reduce
 
 
 class Day13(object):
@@ -69,13 +137,13 @@ class Day13(object):
         with open(input_file_path, "r") as f:
             buses = f.readlines()
         self.timestamp = int(buses[0])
-        self.bus_ids = [int(b) if b != "x" else b for b in buses[1].split(",")]
+        self.bus_ids = np.array([int(b) if b != "x" else 1 for b in buses[1].split(",")])
 
     def part_1(self):
         min_diff = self.timestamp
         bus_id = -1
         for b_id in self.bus_ids:
-            if isinstance(b_id, int):
+            if b_id > 1:
                 diff = abs((self.timestamp % b_id) - b_id)
                 if diff < min_diff:
                     min_diff = diff
@@ -83,5 +151,30 @@ class Day13(object):
 
         return bus_id * min_diff
 
+    @staticmethod
+    def chinese_remainder(n, a):
+        def mul_inv(a, b):
+            b0 = b
+            x0, x1 = 0, 1
+            if b == 1:
+                return 1
+            while a > 1:
+                q = a // b
+                a, b = b, a % b
+                x0, x1 = x1 - q * x0, x0
+            if x1 < 0:
+                x1 += b0
+            return x1
+
+        _sum = 0
+        prod = reduce(lambda a, b: a * b, n)
+        for n_i, a_i in zip(n, a):
+            p = prod // n_i
+            _sum += a_i * mul_inv(p, n_i) * p
+        return _sum % prod
+
     def part_2(self):
-        return 0
+        mod_res = np.array([b - i for i, b in enumerate(self.bus_ids) if b > 1])
+        bus_ids = np.array([b for b in self.bus_ids if b > 1])
+        mod_res[0] = 0
+        return self.chinese_remainder(bus_ids, mod_res)
--- tests/test_day_13.py
+++ tests/test_day_13.py
@@ -10,4 +10,4 @@ def test_part_1(day_13_input):
 def test_part_2(day_13_input):
     solver = Day13(day_13_input)
 
-    assert solver.part_2() == 286
+    assert solver.part_2() == 1068781
